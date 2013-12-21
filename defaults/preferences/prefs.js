59fcdbb012a8b74bdb42d8be503a4d7e8863a2b4
west-mt <west-mt@users.noreply.github.com>
2013-12-21 22:15:49 +0900

    最低限のファイルを登録

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,9 @@
+pref("toolkit.defaultChromeURI", "chrome://ssb/content/ssbrowser.xul");
+
+pref("general.useragent.compatMode.firefox", true);
+
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+//pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
