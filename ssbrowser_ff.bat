39678e0967132e94dbb8ed10834df93a9a38fbb1
west-mt <west-mt@users.noreply.github.com>
2014-02-11 22:55:36 +0900

    .

--- ssbrowser_ff.bat
+++ ssbrowser_ff.bat
@@ -1,2 +1,2 @@
-Start "SSBrowser" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -app application.ini %*
+Start "SSBrowser" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -app application.ini --no-remote -title "Yahoo!" -P default -url http://www.yahoo.co.jp/
 exit
