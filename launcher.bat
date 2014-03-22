6ff90159a97b91277b7c626a2b10985054d81f81
west-mt <west-mt@users.noreply.github.com>
2014-03-22 14:17:00 +0900

    起動バッチファイルを修正

--- launcher.bat
+++ launcher.bat
@@ -4,7 +4,6 @@ rem Start "Launcher" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -app a
 rem Start "Launcher" "C:\Program Files\Mozilla Firefox\firefox.exe" -app application.ini -no-remote -P default -launcher
 
 for %%i in ("C:\Program Files (x86)\Mozilla Firefox\firefox.exe" "C:\Program Files\Mozilla Firefox\firefox.exe") do (
-  if exist %%i  echo Start "Launcher" %%i -app application.ini -no-remote -P default -launcher
+  if exist %%i  Start "Launcher" %%i -app application.ini -no-remote -P default -launcher
 )
-pause
 exit
