200fa782b980a9e6b1a76a7dd3fa091a8ef7aa3c
west-mt <west-mt@users.noreply.github.com>
2014-03-22 14:14:46 +0900

    .

--- launcher.bat
+++ launcher.bat
@@ -1,3 +1,7 @@
-Start "Launcher" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -app application.ini -no-remote -P default -launcher
-Start "Launcher" "C:\Program Files\Mozilla Firefox\firefox.exe" -app application.ini -no-remote -P default -launcher
+rem Start "Launcher" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -app application.ini -no-remote -P default -launcher
+rem Start "Launcher" "C:\Program Files\Mozilla Firefox\firefox.exe" -app application.ini -no-remote -P default -launcher
+
+for %%i in ("C:\Program Files (x86)\Mozilla Firefox\firefox.exe" "C:\Program Files\Mozilla Firefox\firefox.exe") do (
+  if exist %%i  echo Start "Launcher" %%i -app application.ini -no-remote -P default -launcher
+)
 exit
