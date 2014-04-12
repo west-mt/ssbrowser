93ea3f3999b195b50c69658ff9fd6c86cc917602
west-mt <west-mt@users.noreply.github.com>
2014-04-12 14:30:37 +0900

    アップデートプログラムを修正

--- update.py
+++ update.py
@@ -6,10 +6,11 @@ import sys
 import urllib2
 import tempfile
 import zipfile
+import hashlib
 try:
-  import simplejson as json
+    import simplejson as json
 except:
-  import json      
+    import json      
 
 
 def prompt(msg):
@@ -25,6 +26,7 @@ if os.path.exists('.git'):
     sys.exit(3)
 
 info_fname = 'update_info.json'
+binary_fname = 'update.exe'
 
 url = json.load(open(info_fname))
 
@@ -52,6 +54,7 @@ tempf.seek(0)
 zipf = zipfile.ZipFile(tempf)
 
 top_dir_len = -1
+overwrite = False
 
 print 'Finding modified files...'
 for info in zipf.infolist():
@@ -59,20 +62,45 @@ for info in zipf.infolist():
         top_dir_len = len(info.filename)
     else:
         fname = info.filename[top_dir_len:]
+        if fname == binary_fname:
+            print '** %s updated. **' % binary_fname
+            print '  Please download from Github.'
+            continue
         if fname[-1] == '/':
             if not os.path.exists(fname):
-                if prompt('Create new directory "'+fname+'"? (y/N)') == 'y':
+                
+                r = 'n'
+                if not overwrite:
+                    r = prompt('Create new directory "'+fname+'"? (y/N/all)')
+                    if r == 'all':
+                        overwrite = True
+                if r == 'y' or overwrite:
                     os.mkdir(fname)
                     print 'Create: ' + fname
         else:
             if not os.path.exists(fname):
-                if prompt('Create new file "'+fname+'"? (y/N)') == 'y':
+
+                r = 'n'
+                if not overwrite:
+                    r = prompt('Create new file "'+fname+'"? (y/N/all)')
+                    if r == 'all':
+                        overwrite = True
+                if r == 'y' or overwrite:
                     data = zipf.read(info)
                     open(fname, 'wb').write(data)
                     print 'Create: ' + fname
             else:
-                if os.path.getsize(fname) != info.file_size:
-                    if prompt('Update modified file "'+fname+'"? (y/N)') == 'y':
+                zip_md5 = hashlib.md5(zipf.read(info)).hexdigest()
+                orig_md5 = hashlib.md5(open(fname, 'rb').read()).hexdigest()
+
+                if zip_md5 != orig_md5:
+
+                    r = 'n'
+                    if not overwrite:
+                        r = prompt('Update modified file "'+fname+'"? (y/N/all)')
+                        if r == 'all':
+                            overwrite = True
+                    if r == 'y' or overwrite:
                         data = zipf.read(info)
                         open(fname, 'wb').write(data)
                         print 'Update: ' + fname
